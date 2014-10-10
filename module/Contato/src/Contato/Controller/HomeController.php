b7a6c2fc21c1b5f704b21c49094b5d9107eef842
André Souza <Andr Souza@Andre>
2014-10-10 19:15:20 -0300

    cricção do controller contato

--- module/Contato/src/Contato/Controller/HomeController.php
+++ module/Contato/src/Contato/Controller/HomeController.php
@@ -14,6 +14,15 @@ class HomeController extends AbstractActionController
         
         return new ViewModel();
     }
+    
+    /**
+     * action sobre
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function sobreAction()
+    {
+        return new ViewModel();
+    }
 
 
 }
