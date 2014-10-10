7c091abd7677f9aff9bf8d579d6cec0b647f5005
André Souza <Andr Souza@Andre>
2014-10-09 23:02:17 -0300

    criação do esqueleto basico da aplicação

---
+++ module/Contato/src/Contato/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Contato\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+
+class HomeController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        
+        return new ViewModel();
+    }
+
+
+}
+
